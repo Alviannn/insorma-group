723e798f8b60362c4c3c9e749dc1841bd71e1ec2
Juan Trilnardo Defrin <103166887+juantdefrin94@users.noreply.github.com>
2022-06-13 17:54:02 +0700

    add fragment for mainpage, transactionpage, and profilepage

--- app/src/main/java/dev/gafilianog/insorma/MainActivity.java
+++ app/src/main/java/dev/gafilianog/insorma/MainActivity.java
@@ -1,8 +1,11 @@
 package dev.gafilianog.insorma;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void toMainPage(View view) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.container, new MainPage());
+        fragmentTransaction.commit();
+    }
+
+    public void toTransactionPage(View view) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.container, new TransactionPage());
+        fragmentTransaction.commit();
+    }
+
+    public void toProfilePage(View view) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.container, new ProfilePage());
+        fragmentTransaction.commit();
     }
 }
