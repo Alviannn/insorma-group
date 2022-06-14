54f59251fe985649cf50610bad2db5c5d102f484
Alviannn <alvianndq@gmail.com>
2022-06-14 14:27:37 +0700

    Add global SQL date formatter

--- app/src/main/java/dev/juviga/insorma/data/shared/SharedData.java
+++ app/src/main/java/dev/juviga/insorma/data/shared/SharedData.java
@@ -1,9 +1,19 @@
 package dev.juviga.insorma.data.shared;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
 import dev.juviga.insorma.data.db.DatabaseHelper;
 
 public class SharedData {
 
     public static DatabaseHelper DATABASE_HELPER;
+    public final static DateFormat SQL_DATE_FORMATTER;
+
+    static {
+        String dateFormat = "yyyy-MM-dd HH:mm:ss";
+        SQL_DATE_FORMATTER = new SimpleDateFormat(dateFormat, Locale.getDefault());
+    }
 
 }
