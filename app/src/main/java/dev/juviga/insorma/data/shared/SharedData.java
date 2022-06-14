73a275b6265e03027c73204bb1a0a0edd5f617ff
Alviannn <alvianndq@gmail.com>
2022-06-14 13:24:33 +0700

    Add global db helper instance

---
+++ app/src/main/java/dev/juviga/insorma/data/shared/SharedData.java
@@ -0,0 +1,9 @@
+package dev.juviga.insorma.data.shared;
+
+import dev.juviga.insorma.data.db.DatabaseHelper;
+
+public class SharedData {
+
+    public static DatabaseHelper DATABASE_HELPER;
+
+}
--- app/src/main/java/dev/juviga/insorma/ui/LandingActivity.java
+++ app/src/main/java/dev/juviga/insorma/ui/LandingActivity.java
@@ -1,10 +1,12 @@
 package dev.juviga.insorma.ui;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import dev.juviga.insorma.R;
+import dev.juviga.insorma.data.db.DatabaseHelper;
+import dev.juviga.insorma.data.shared.SharedData;
 
 public class LandingActivity extends AppCompatActivity {
 
@@ -12,5 +14,15 @@ public class LandingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
+
+        SharedData.DATABASE_HELPER = new DatabaseHelper(getApplicationContext());
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        SharedData.DATABASE_HELPER.close();
+    }
+
 }
